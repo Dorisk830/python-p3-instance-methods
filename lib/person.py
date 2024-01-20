a82c5f0cc4633d00a6cf565a68bd1d25a5000485
Doris <doris.kiprop@student.moringaschool.com>
2024-01-20 17:46:58 +0300

    passed

--- lib/person.py
+++ lib/person.py
@@ -3,5 +3,11 @@
 class Person:
     # Class body goes here
 
-    #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+    def walk(self):
+        print("The person is walking.")
+
+doris=Person()
+doris.talk
+doris.walk
